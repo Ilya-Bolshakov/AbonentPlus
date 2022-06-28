9734e736492fe12f73a2d9c6e3268da8cc00983b
Илья <gamenest926@gmail.com>
2022-06-28 23:33:13 +0300

    Added form for Disrepair

---
+++ Practice1_MongoDB_With_On_WinForms/Practice1_MongoDB_With_On_WinForms/forms/Form_Disrepair.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Practice1_MongoDB_With_On_WinForms.forms
+{
+    partial class Form_Disrepair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(22, 397);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(107, 34);
+            this.btn_Delete.TabIndex = 7;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(22, 282);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(107, 34);
+            this.btn_Add.TabIndex = 6;
+            this.btn_Add.Text = "Add new abonent";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Location = new System.Drawing.Point(22, 341);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(107, 34);
+            this.btn_Edit.TabIndex = 5;
+            this.btn_Edit.Text = "Edit abonent";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(800, 230);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // Form_Disrepair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form_Disrepair";
+            this.Text = "Form_Disrepair";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ Practice1_MongoDB_With_On_WinForms/Practice1_MongoDB_With_On_WinForms/forms/Form_Disrepair.cs
@@ -0,0 +1,35 @@
+﻿using Practice1_MongoDB_With_On_WinForms.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice1_MongoDB_With_On_WinForms.forms
+{
+    public partial class Form_Disrepair : Form
+    {
+        private readonly MongoDBContext context;
+        private BindingList<Disrepair> _bindList;
+        public Form_Disrepair()
+        {
+            InitializeComponent();
+
+            context = MongoDBContext.GetObject();
+            _bindList = context.Load_DataDisrepair();
+            dataGridView.DataSource = _bindList;
+
+            // формируем список атрибутов для фильтрации
+            //IList<string> vs = new List<string>();
+            //foreach (DataGridViewColumn item in dataGridView.Columns)
+            //{
+            //    vs.Add(item.Name);
+            //}
+            //cb_Filters.DataSource = vs;
+        }
+    }
+}
