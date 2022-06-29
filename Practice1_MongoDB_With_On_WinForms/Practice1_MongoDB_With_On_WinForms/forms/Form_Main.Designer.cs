6de30538cbd01dbbf048deb90c5b4f97bd987c5c
Илья <gamenest926@gmail.com>
2022-06-29 22:15:56 +0300

    Revert "test"
    This reverts commit ba3c83bd55eb29cc08663250a901168452948ed6.

--- Practice1_MongoDB_With_On_WinForms/Practice1_MongoDB_With_On_WinForms/forms/Form_Main.Designer.cs
+++ Practice1_MongoDB_With_On_WinForms/Practice1_MongoDB_With_On_WinForms/forms/Form_Main.Designer.cs
@@ -31,7 +31,6 @@
             this.btn_Form_Abonent = new System.Windows.Forms.Button();
             this.btn_Form_Disrepair = new System.Windows.Forms.Button();
             this.btn_Form_Request = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_Form_Abonent
@@ -70,29 +69,18 @@
             this.btn_Form_Request.UseVisualStyleBackColor = false;
             this.btn_Form_Request.Click += new System.EventHandler(this.btn_Form_Request_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
-            // 
             // Form_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(801, 366);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Form_Request);
             this.Controls.Add(this.btn_Form_Disrepair);
             this.Controls.Add(this.btn_Form_Abonent);
             this.Name = "Form_Main";
             this.Text = "Form_Main";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -101,6 +89,5 @@
         private System.Windows.Forms.Button btn_Form_Abonent;
         private System.Windows.Forms.Button btn_Form_Disrepair;
         private System.Windows.Forms.Button btn_Form_Request;
-        private System.Windows.Forms.Label label1;
     }
 }
